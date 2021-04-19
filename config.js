d64849a34ade14a85a9776bedf83ed6d19ed1c70
Bhanu K <bhanu.kamapantula@gramener.com>
2021-04-19 08:12:15 +0530

    config script

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  "toppanel": {
+    "links": [
+      {"text": "By Suresh Ediga", "url": "https://twitter.com/suresh_ediga"},
+      {"text": "Data", "url": "https://docs.google.com/spreadsheets/d/1-Df0fLteX567HFkHjyYfxnUhqjUz9krafU27zLcqPrM/edit?usp=sharing"}
+    ],
+    "title": "Covid-19 Emergency Contact Information"
+  },
+  "cover": "Emergency contact information for oxygen, medicines and doctors"
+}
