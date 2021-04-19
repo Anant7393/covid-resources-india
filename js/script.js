ad6a912afcafcf2a328e0c18d097f5d0520f19ff
Bhanu K <bhanu.kamapantula@gramener.com>
2021-04-19 10:32:15 +0530

    ENH: table view, search by text

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@ function toggle_state_cards(val, param) {
     // partial matches of categories should work
     $(".state-card[data-category*='" + val + "']").show()
     $(".state-card[data-category*='" + val + "']").siblings("h4").show()
-    $('.events-counter').html($('.state-card[data-category*="'+ val + '"]').length + ' updates')
+    $('.events-counter').html($('.state-card[data-category*="'+ val + '"]').length)
   } else {
-    $('.events-counter').html($(".state-card[data-labeltarget='" + val + "']").length + ' updates')
+    $('.events-counter').html($(".state-card[data-labeltarget='" + val + "']").length)
     $(".state-card[data-labeltarget='" + val + "']").show()
     $(".state-card[data-labeltarget='" + val + "']").siblings("h4").show()
   }
@@ -25,7 +25,7 @@ function toggle_state_cards(val, param) {
   * @param {string} arrange_by
 */
 function render_page(arrange_by) {
-  $('.events-counter').html(sheets_global.length + ' updates')
+  $('.events-counter').html(sheets_global.length)
   if(arrange_by === 'date') {
     filtered_elements_dates = _.sortBy(
       _.each(sheets_global, function(item) { return item['dt'] = new Date(Number(Date.parse(item.when).toString()))
@@ -39,11 +39,21 @@ function render_page(arrange_by) {
   }
   var tmpl = _.template($("#item-template").html())
   $('#updates').html(tmpl(data))
+  $('.formhandler').formhandler({
+    data: sheets_global,
+    sort: false,
+    pageSize: 10,
+    columns: [{link: false, name: '*'}, {name: 'Timestamp', hide: true}],
+    export: false,
+    count: false
+  })
+  $('body').search()
+  var el = document.querySelector('.tabs')
+  M.Tabs.init(el)
 }
 
 function render_static_content() {
   var tmpl = _.template($("#static-content").html())
-  console.log("config", config)
   $("#content").html(tmpl({config: config}))
 }
 
@@ -65,8 +75,8 @@ function init(arrange_by) {
 
   $('body').urlfilter({target: 'pushState'})
 
-  var elems = document.querySelectorAll('.sidenav')
-  M.Sidenav.init(elems)
+  var elements = document.querySelectorAll('.sidenav')
+  M.Sidenav.init(elements)
 }
 window.addEventListener('DOMContentLoaded', init)
 
@@ -83,7 +93,7 @@ $('body').on('click', 'a.btn', function() {
   $('.state-card').siblings("h4").show()
   $("#autocomplete").val('')
   $("[data-label]").removeClass('border-selected')
-  $('.events-counter').html($('.state-card').length + ' updates')
+  $('.events-counter').html($('.state-card').length)
   g1_url.update({cat: null, view: null}, 'del')
   history.pushState({}, '', g1_url.toString())
 }).on('click', '[data-label]', function() {
